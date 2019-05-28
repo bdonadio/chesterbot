2e0f3e6b066a8381418bdc7952efe662d8bff9ea
Brian Donadio <bdonadio@users.noreply.github.com>
2019-05-27 22:38:39 -0400

    Add files via upload

--- skills/help.js
+++ skills/help.js
@@ -5,6 +5,7 @@ module.exports = function (controller) {
 
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
+        text += "\n- " + bot.appendMention(message, "favorite") + ": user picked favorite Cheetos flavor is stored";
         text += "\n- " + bot.appendMention(message, "color") + ": ask to pick a random color";
         text += "\n- " + bot.appendMention(message, "loop") + ": example of a menu that loops until explicitly stopped";
         text += "\n- " + bot.appendMention(message, "menu") + ": implement a menu via a conversation";
