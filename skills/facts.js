eeb7503e999f02ffa9854f86aac7a71ae262501d
Brian Donadio <bdonadio@users.noreply.github.com>
2019-06-22 22:24:13 -0400

    Add files via upload

--- skills/facts.js
+++ skills/facts.js
@@ -1,93 +1,64 @@
-
-
 //
-// Threaded conversation that searches for Cheetos food product based on entered search term in USDA Database
+// Illustrates a muti-threaded conversation
+//
+// Q: "What about coffee (yes / no / cancel)"
+// A: no
+// Q: "What would you like to drink?"
+// A: Coke
 //
 module.exports = function (controller) {
 
-    controller.hears([/^facts$/], "direct_message,direct_mention", function (bot, message) {
+    controller.hears([/^facts$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
 
-            convo.ask("Enter Cheetos product name to lookup nutrition information", [
+            convo.ask("What about coffee (yes/**no**/cancel)", [
                 {
-                //    pattern: "^blue|green|pink|red|yellow$",
+                    pattern: "yes|yeh|sure|oui|si",
+                    callback: function (response, convo) {
+                        convo.say("Go, get some !");
+                        convo.next();
+                    },
+                }
+                , {
+                    pattern: "no|neh|non|na|birk",
+                    callback: function (response, convo) {
+                        convo.gotoThread('ask_drink');
+                    },
+                }
+                , {
+                    pattern: "cancel|stop|exit",
+                    callback: function (response, convo) {
+                        convo.say("Got it, cancelling...");
+                        convo.next();
+                    },
+                }
+                , {
                     default: true,
                     callback: function (response, convo) {
-                        convo.gotoThread("confirm_choice");
-                    },
-                },
-        //        {
-          //          default: true,
-          //          callback: function (response, convo) {
-            //            convo.gotoThread('bad_response');
-              //      }
-          //      }
-            ], { key: "answer" });
+                        // We've got 2 options at this point:
 
+                        // 1. simply repeat the question
+                        //convo.repeat();
+                        //convo.next();
 
-            // Bad response
-            //convo.addMessage({
-              //  text: "Sorry, I don't know this color.<br/>_Tip: try blue, green, pink, red or yellow!_",
-                //action: 'default',
-            //}, 'bad_response');
-
-            // Confirmation thread
-            convo.addMessage(
-                "You picked '{{responses.answer}}'",
-                "confirm_choice");
-
-            convo.addQuestion("Please, confirm your choice ? (yes|no)", [
-                {
-                    pattern: "^yes|hey|oui|si|da$",
-                    callback: function (response, convo) {
-                        var pickedFlavor = convo.extractResponse('answer');
-                        convo.setVar("flavor", pickedFlavor);
-                        convo.gotoThread("productSearch");
-                    },
-                },
-                {
-                    default: true,
-                    callback: function (response, convo) {
-                        convo.transitionTo("default", "Got it, let's try again...");
+                        // 2. or provide extra info, then repeat the question
+                        convo.gotoThread("bad_response");
                     }
                 }
-            ], {}, "confirm_choice");
+            ]);
 
+            // Bad response
+            convo.addMessage({
+                text: "Sorry, I did not understand!<br/>_Tip: try 'yes', 'no' or 'cancel._'",
+                action: 'default', // goes back to the thread's current state, where the question is not answered
+            }, 'bad_response');
 
-            //Product Search Thread
-            convo.addMessage(
-                "I'm searching for '{{vars.flavor}}'",
-
-
-                //Rest API Search Request to USDA Food Database
-                var request = require("request");
-
-                var options = { method: 'GET',
-                  url: 'https://api.nal.usda.gov/ndb/search/',
-                  qs:
-                   { format: 'json',
-                     q: 'pickedFlavor',
-                     sort: 'n',
-                     max: '25',
-                     offset: '0',
-                     api_key: 'wFqUmW1ckj0MTlwYUxmgx5XsOlboTvwJceKcCzIf' },
-                  headers:
-                   { 'Postman-Token': '106ade00-4e99-4323-9cee-af37535fb3bc',
-                     'cache-control': 'no-cache' } };
-
-                request(options, function (error, response, body) {
-                  if (error) throw new Error(error);
-
-                  console.log(body);
-                });
-                "productSearch");
-
-
-            // Success thread
-            convo.addMessage(
-                "Cool, I love '{{vars.flavor}}' too",
-                "success");
+            // Thread: ask for a drink
+            convo.addQuestion('What would you like to drink?', function (response, convo) {
+                convo.say(`I love '${response.text}' too`);
+                convo.next();
+            }, {}, 'ask_drink');
         });
     });
 };
