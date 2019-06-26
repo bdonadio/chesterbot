ec34cd785f5da6d4fc0f59a24d17f911f01de161
Brian Donadio <bdonadio@users.noreply.github.com>
2019-06-25 21:36:42 -0400

    Add files via upload

---
+++ skills/cheetosinfo.js
@@ -0,0 +1,73 @@
+//
+// Example of a conversation with a menu that loops until explicitly stopped
+//
+module.exports = function (controller) {
+
+    controller.hears([/^cheetosinfo$/], 'direct_message,direct_mention', function (bot, message) {
+
+        bot.startConversation(message, function (err, convo) {
+
+            var question = "Here are some helpful Cheetos activities I can help you with:";
+            question += "<br/> `1)` get information on Cheetos Products";
+            question += "<br/> `2)` find information about Cheetos Recipes";
+            question += "\n\nWhat do you want to check?<br/>_(type a number, a **bold keyword** or `stop`)_";
+            convo.ask(question, [
+                {
+                    pattern: "1|Products",
+                    callback: function (response, convo) {
+                        convo.gotoThread('menu_1');
+                    },
+                }
+                , {
+                    pattern: "2|Recipes",
+                    callback: function (response, convo) {
+                        convo.gotoThread('menu_2');
+                    },
+                }
+                , {
+                    pattern: "cancel|stop",
+                    callback: function (response, convo) {
+                        convo.gotoThread('action_cancel');
+                    },
+                }
+                , {
+                    default: true,
+                    callback: function (response, convo) {
+                        convo.gotoThread('bad_response');
+                    }
+                }
+            ]);
+
+            // Menu option 1)
+            convo.addMessage({
+                text: "White Cheddar Bites https://www.cheetos.com/products/cheetos-white-cheddar-bites-cheese-flavored-snacks",
+                action: 'default'
+            }, 'menu_1');
+
+            // Menu option 2)
+            convo.addMessage({
+                text: "Learnings **labs** are step-by-step tutorials. They are grouped into **tracks** to help you on your rampup journey. Just browse through [the learnings tracks](https://learninglabs.cisco.com/login) and pick the labs that suits your learning appetite!",
+                action: 'default'
+            }, 'menu_2');
+
+            // Menu option 3)
+            convo.addMessage({
+                text: "Nothing's like meeting in person at a conference, training or a hackathon. Check the list of [DevNet events](https://developer.cisco.com/site/devnet/events-contests/events/) or ask the bot: invite `CiscoDevNet@sparkbot.io` to chat in a Webex Teams space.",
+                action: 'default'
+            }, 'menu_3');
+
+            // Cancel
+            convo.addMessage({
+                text: "Got it, cancelling...",
+                action: 'stop', // this marks the converation as unsuccessful
+            }, 'action_cancel');
+
+            // Bad response
+            convo.addMessage({
+                text: "Sorry, I did not understand.",
+                action: 'default',
+            }, 'bad_response');
+
+        });
+    });
+};
