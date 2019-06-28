773b42710a3976730eb599c08ea9a68660cac437
Brian Donadio <bdonadio@users.noreply.github.com>
2019-06-27 23:07:30 -0400

    Add files via upload

--- skills/help.js
+++ skills/help.js
@@ -6,17 +6,7 @@ module.exports = function (controller) {
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
         text += "\n- " + bot.appendMention(message, "favorite") + ": user picked favorite Cheetos flavor is stored";
-        text += "\n- " + bot.appendMention(message, "facts") + ": returns nutrition facts for searched Cheetos product";
-        text += "\n- " + bot.appendMention(message, "cheetosinfo") + ": provides information about cheetoes products and recipes";
         text += "\n- " + bot.appendMention(message, "products") + ": provides information about cheetos products";
-        text += "\n- " + bot.appendMention(message, "color") + ": ask to pick a random color";
-        text += "\n- " + bot.appendMention(message, "loop") + ": example of a menu that loops until explicitly stopped";
-        text += "\n- " + bot.appendMention(message, "menu") + ": implement a menu via a conversation";
-        text += "\n- " + bot.appendMention(message, "quiz") + ": multi-threaded conversation with timeout";
-        text += "\n- " + bot.appendMention(message, "restricted") + ": let a user pick a color among a set of options";
-        text += "\n- " + bot.appendMention(message, "storage") + ": store picked color as a user preference";
-        text += "\n- " + bot.appendMention(message, "threads") + ": branch to another thread";
-        text += "\n- " + bot.appendMention(message, "variables") + ": enriched user-context among threads";
         text += "\n\nI also understand:";
         text += "\n- " + bot.appendMention(message, "about") + ": shows metadata about myself";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
